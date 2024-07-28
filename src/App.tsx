000a1912f1ced5af0544af3a115f6ee86e49764b
Keshav Dussal <ksdussal96@gmail.com>
2024-07-28 11:05:36 +0530

    show and hide functionality with toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [textState,setTextState] = useState(false);
+  const handleToggle = ()=>{
+    setTextState(!textState)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={handleToggle}>Toggle Text</button>
+        {
+          textState?
+          <p>Demo Text</p>
+          :""
+        }
     </div>
   );
 }
