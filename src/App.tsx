c9d581842cfd1efa31a5c61944d2be8df5f2610e
Keshav Dussal <ksdussal96@gmail.com>
2024-07-28 18:14:12 +0530

    Accept form data and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, useState } from 'react';
 import './App.css';
-
+interface FormData{
+  firstName:string,
+  lastName:string
+}
 function App() {
+  const [formData,setFormData] = useState<FormData>({
+    firstName:"",
+    lastName:""
+  });
+  const [showData,setShowData] = useState(false);
+
+  const handleChange = (event:ChangeEvent<HTMLInputElement>)=>{
+    const {name,value} = event.target;
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+
+  }
+ 
+  const handleSubmit = (event:any)=>{
+    event.preventDefault();
+    setShowData(true)
+    console.log("inside handleSubmit");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <label htmlFor='fn'>FirstName:</label><input type='text' onChange={handleChange} name="firstName" value={formData.firstName} required/><br/>
+        <label htmlFor='ln'>LastName:</label><input type='text' onChange={handleChange} name="lastName" value={formData.lastName} required/><br/>
+        <button onClick={handleSubmit}>Submit Form</button>
+        {
+          formData.firstName && formData.lastName && showData?(
+          <div>
+            <p>Form Data:</p>
+            <p>FirstName:{formData.firstName}</p>
+            <p>LastName:{formData.lastName}</p>
+          </div>
+        ):""
+        }
     </div>
   );
 }
